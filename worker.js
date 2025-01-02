d518da49c66682f32ea01352635e54cdf34c231f
ayanbodematthew <135076288+ayanbodematthew@users.noreply.github.com>
2025-01-02 07:54:11 +0100

    Add files via upload

--- worker.js
+++ worker.js
@@ -1,15 +1,18 @@
-const cacheFiles = ["/Audio.html", "/Audio.js", "/Screenshot_20241216-125252_2.png"]
+const cacheFiles = ["/Audio.html", "/Audio.js"]
 
 const cacheName = "v3";
+const cacheName2 = "video_cache_v1";
 
-self.addEventListener("install", (event) => {
+const dura = 7 * 24 * 60 * 60 * 1000;
+
+self.addEventListener("install", function(event) {
     event.waitUntil(caches.open(cacheName).then(cache => {
         console.log("Worker adding file to cache: ", cache)
         return cache.addAll(cacheFiles)
     }))
 })
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function(event) {
     event.waitUntil(caches.keys().then(cacheNames => {
         return Promise.all(cacheNames.map(thisCacheName => {
             if (thisCacheName !== cacheName) {
@@ -20,11 +23,20 @@ self.addEventListener("activate", (event) => {
     }))
 })
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function(event) {
+
     if (event.request.method !== "GET") {
-        console.log("Not to cache")
         return;
     }
+
+    var url = new URL(event.request.url)
+    if (url.origin == "https://www.googleapis.com") {
+
+        event.respondWith(cacheFirstWithExpiration(event.request))
+        return;
+
+    }
+
     event.respondWith(fetch(event.request).then(response => {
         const clone = response.clone();
         caches.open(cacheName).then(cache => {
@@ -41,3 +53,30 @@ self.addEventListener("fetch", (event) => {
         })
     }))
 })
+
+async function cacheFirstWithExpiration(req) {
+
+    const cache = await caches.open(cacheName2)
+    const resp = cache.match(req)
+
+    if (resp) {
+
+        const cacheTime = new Date(resp.headers.get("date")).getTime()
+        const now = Date.now();
+
+        if (now - cacheTime < dura) {
+            return resp;
+        } else {
+            await caches.delete(req);
+        }
+
+    }
+
+    const net_resp = await fetch(req);
+    if (net_resp.ok) {
+        const cloned = net_resp.clone()
+        cache.put(req, cloned);
+    }
+    return net_resp;
+
+}
